cbe6bb3c12a2f01817d2c24e02675525feb45951
fischgeek <fischgeek@gmail.com>
2021-05-24 11:07:08 -0500

    cleanup

--- DotNetDates.FSharp/FormatSpecifiers.fs
+++ DotNetDates.FSharp/FormatSpecifiers.fs
@@ -11,22 +11,6 @@ module FormatSpecifiers =
     let private fmt (s: string) = toFormat $"""ToString("{s}")""" (DateTime.Now.ToString(s))
 
     let AddSpecifiers (flp: FlowLayoutPanel) = 
-        flp.Controls.Add(fmt "d")
-        flp.Controls.Add(fmt "D")
-        flp.Controls.Add(fmt "f")
-        flp.Controls.Add(fmt "F")
-        flp.Controls.Add(fmt "g")
-        flp.Controls.Add(fmt "G")
-        flp.Controls.Add(fmt "G")
-        flp.Controls.Add(fmt "M")
-        flp.Controls.Add(fmt "o")
-        flp.Controls.Add(fmt "O")
-        flp.Controls.Add(fmt "r")
-        flp.Controls.Add(fmt "R")
-        flp.Controls.Add(fmt "s")
-        flp.Controls.Add(fmt "t")
-        flp.Controls.Add(fmt "T")
-        flp.Controls.Add(fmt "u")
-        flp.Controls.Add(fmt "U")
-        flp.Controls.Add(fmt "y")
-        flp.Controls.Add(fmt "Y")
+        ["d";"D";"f";"F";"g";"G";"G";"M";"o";"O";"r";"R";"s";"t";"T";"u";"U";"y";"Y"]
+        |> Seq.map fmt
+        |> Seq.iter flp.Controls.Add
